0e499a9abd4b06180d59099ed157fedd000770e9
Jason Dillon <jason@planet57.com>
2013-10-09 16:08:19 -0700

    Add NX.util.DateFormat helper

---
+++ plugins/internal/nexus-rapture-plugin/src/main/resources/static/rapture/NX/util/DateFormat.js
@@ -0,0 +1,54 @@
+Ext.define('NX.util.DateFormat', {
+  singleton: true,
+
+  mixins: [
+    'NX.LogAware'
+  ],
+
+  /**
+   * @private
+   */
+  defaultPatterns: {
+    date: {
+      'short': 'Y-M-d',       // 2013-Mar-06
+      'long': 'l, F d, Y'     // Wednesday, March 06, 2013
+    },
+
+    time: {
+      'short': 'H:i:s',                   // 15:49:57
+      'long': 'H:i:s T (\\G\\M\\TO)'      // 15:49:57-0700 PST (GMT-0700)
+    },
+
+    datetime: {
+      'short': 'Y-M-d H:i:s',                     // 2013-Mar-06 15:49:57
+      'long': 'l, F d, Y H:i:s T (\\G\\M\\TO)'    // Wednesday, March 06, 2013 15:50:19 PDT (GMT-0700)
+    }
+  },
+
+  /**
+   * Return the date format object for the given name.
+   *
+   * Date format objects currently have a 'short' and 'long' variants.
+   *
+   *      @example
+   *      var longDatetimePattern = NX.util.DateFormat.forName('datetime')['long'];
+   *      var shortDatePattern = NX.util.DateFormat.forName('date')['short'];
+   *
+   * @param name
+   *
+   * @return {*} Date format object.
+   */
+  forName: function (name) {
+    var format = this.defaultPatterns[name];
+
+    // if no format, complain and return the full ISO-8601 format
+    if (!name) {
+      this.logWarn('Missing named format:', name);
+      return 'c';
+    }
+
+    // TODO: Eventually let this customizable by user, for now its hardcoded
+
+    return format;
+  }
+});
