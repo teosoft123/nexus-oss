d5cd95d0f0f4a09326cddd24cd4d0ca116b66c0b
Jason Dillon <jason@planet57.com>
2013-10-09 14:39:14 -0700

    replace bash script with groovy, add to maven build

--- plugins/internal/nexus-rapture-baseapp/README.md
+++ plugins/internal/nexus-rapture-baseapp/README.md
@@ -1,12 +1,19 @@
-= Nexus Rapture Baseapp
+# Nexus Rapture Baseapp
 
 This module provides the baseapp muck for ExtJS 4+ pre-compiled with Sencha CMD.
 
 Requires Sencha CMD 4+
 
-Recompile is manual via build.sh script ATM, afterwards the changes should be committed.
-
 When updating baseapp/ext need to be aware there are directories named 'target':
 * ext/src/fx/target
 
 May completely revisit this solution later.
+
+## Regenerating
+
+    mvn clean install -Pregenerate
+
+If the content has changed, then the result needs to be commited:
+
+    git commit . -m "regenerated baseapp"
+
---
+++ plugins/internal/nexus-rapture-baseapp/src/main/script/regenerate.groovy
@@ -0,0 +1,59 @@
+//
+// Regenerates the baseapp js/css/images for the current configuration/style.
+//
+
+println ''
+println 'Prepare:'
+
+// ensure we can execute Sencha CMD
+ant.exec(executable: 'sencha', failonerror: true) {
+  arg(line: 'which')
+}
+
+[ 'production', 'testing' ].each { flavor ->
+  println "Regenerating: $flavor"
+
+  // run 'app build' for each flavor
+  ant.mkdir(dir: "target/$flavor")
+  ant.exec(executable: 'sencha', dir: 'src/main/baseapp', failonerror: true, output: "target/$flavor/build.log") {
+    arg(line: "app build $flavor")
+  }
+}
+
+println ''
+println 'Installing:'
+
+// outdir is under src/main
+def outdir = new File('../resources/static/rapture').canonicalFile
+println "Output directory: $outdir"
+
+ant.mkdir(dir: outdir)
+ant.mkdir(dir: "$outdir/resources")
+ant.mkdir(dir: "$outdir/resources/images")
+
+// install non-debug files
+ant.copy(file: 'target/production/BaseApp/app.js', tofile: "$outdir/baseapp.js")
+ant.copy(file: 'target/production/BaseApp/resources/BaseApp-all.css', tofile: "$outdir/resources/baseapp.css")
+
+// install debug files
+ant.copy(file: 'target/testing/BaseApp/app.js', tofile: "$outdir/baseapp-debug.js")
+ant.copy(file: 'target/testing/BaseApp/resources/BaseApp-all.css', tofile: "$outdir/resources/baseapp-debug.css")
+
+// install non-debug images
+ant.copy(todir: "$outdir/resources/images", overwrite: true) {
+  fileset(dir: "target/production/BaseApp/resources/images") {
+    include(name: '**')
+  }
+}
+
+println ''
+println 'Changes:'
+ant.exec(executable: 'git') {
+  arg(line: 'status -s .')
+}
+
+println ''
+println 'Done'
+println ''
+
+
