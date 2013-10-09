9ef417ed8cf83a7cd70cb5c003f866c9160e6714
Jason Dillon <jason@planet57.com>
2013-10-08 17:47:43 -0700

    optimize

--- plugins/internal/nexus-rapture-plugin/src/main/baseapp/bootstrap.js
+++ plugins/internal/nexus-rapture-plugin/src/main/baseapp/bootstrap.js
@@ -416,7 +416,6 @@ Ext.ClassManager.addNameAlternateMappings({
   ],
   "Ext.grid.RowEditor": [],
   "Ext.grid.RowEditorButtons": [],
-  "Ext.grid.Scroller": [],
   "Ext.grid.View": [],
   "Ext.grid.ViewDropZone": [],
   "Ext.grid.column.Action": [
@@ -1303,7 +1302,6 @@ Ext.ClassManager.addNameAliasMappings({
   "Ext.grid.RowEditorButtons": [
     "widget.roweditorbuttons"
   ],
-  "Ext.grid.Scroller": [],
   "Ext.grid.View": [
     "widget.gridview"
   ],
